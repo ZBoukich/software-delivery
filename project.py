4432789e35b1ecaf2c380ab10caaa5f7cf760c4a
Zahey <z.boukich@gmail.com>
2016-06-13 19:44:03 +0200

    remove redis

--- project.py
+++ project.py
@@ -1,20 +1,15 @@
 from flask import Flask
-from redis import Redis
 import os
 import socket
 
 app = Flask(__name__)
-redis = Redis(host=os.environ.get('REDIS_HOST', 'redis'), port=6379)
-
 
 
 @app.route('/test/redis')
 def hello_world():
-    visits=redis.incr('counter',2)
-    html= "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}<br/>"
-    return html.format(name=os.getenv('NAME', "world"), hostname=socket.gethostname(), visits=visits)
+
+
+    return "works"
 
 
 @app.route('/')
